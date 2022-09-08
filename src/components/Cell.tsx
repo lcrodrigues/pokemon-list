8e75b5e436b2af60c2077c25ae170dac012e9974
Leonardo Rodrigues <leonardo.rodrigues@poatek.com>
2022-09-08 17:46:22 -0300

    Turns Cell into a clickable component and apply styled components on it

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,31 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { capitalizeFirstLetter } from "../utils/functions/stringFunctions";
 import Favorite from "./Favorite";
 
-interface CellProps {
-  title: string;
+import styled from "styled-components/native";
+import { TouchableOpacityProps, ViewProps } from "react-native";
+
+interface ContainerProps {
+  index: number;
 }
 
-const Cell = ({ title }: CellProps) => {
+const Container = styled.TouchableOpacity<ContainerProps>`
+  padding: 16px;
+  margin: 8px 0px;
+  border-radius: 8px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${(props: ContainerProps) =>
+    props.index % 3 == 0
+      ? "#8CCE7C"
+      : props.index % 3 === 1
+      ? "#F66666"
+      : "#66C2F6"};
+`;
+
+const Text = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+  color: white;
+`;
+
+interface CellProps extends TouchableOpacityProps {
+  title: string;
+  index: number;
+}
+
+const Cell = ({ title, index, onPress }: CellProps) => {
   return (
-    <View style={styles.container}>
+    <Container index={index} onPress={onPress}>
       <Text>{capitalizeFirstLetter(title)}</Text>
       <Favorite isFavorite={false} />
-    </View>
+    </Container>
   );
 };
 
 export default Cell;
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    marginVertical: 8,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderRadius: 8,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-});
