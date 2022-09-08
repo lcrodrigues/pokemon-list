0f50276e8d8d731417ae9d1354d54ea00821dda9
Leonardo Rodrigues <leonardo.rodrigues@poatek.com>
2022-09-08 17:53:13 -0300

    Navigate to DetailsScreen

--- src/screens/PokemonListScreen.tsx
+++ src/screens/PokemonListScreen.tsx
@@ -4,8 +4,11 @@ import Cell from "../components/Cell";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPokemon } from "../store/pokemon/pokemonListSlice";
 import { AppDispatch, RootState } from "../store/store";
+import { FilledPokeball } from "../../assets/icons";
+import { PokedexProps } from "../navigation/PokedexNavigator";
+import { PokemonResult } from "../models/PokemonResult";
 
-const PokemonListScreen = () => {
+const PokemonListScreen = ({ navigation, route }: PokedexProps) => {
   const handleOnEndReached = () => {
     const { loading, offset, pokemon } = screenState;
 
@@ -18,6 +21,10 @@ const PokemonListScreen = () => {
 
   const dispatch = useDispatch<AppDispatch>();
 
+  const navigateToDetails = (pokemon: PokemonResult) => {
+    navigation.navigate("DetailsScreen", { pokemon });
+  };
+
   useEffect(() => {
     dispatch(fetchPokemon({ offset: 0 }));
   }, []);
@@ -27,7 +34,13 @@ const PokemonListScreen = () => {
     <FlatList
       style={styles.list}
       data={screenState.pokemon.results}
-      renderItem={({ item }) => <Cell title={item.name} />}
+      renderItem={({ item, index }) => (
+        <Cell
+          onPress={() => navigateToDetails(item)}
+          index={index}
+          title={item.name}
+        />
+      )}
       onEndReached={() => handleOnEndReached()}
     />
   );
