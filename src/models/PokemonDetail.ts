ad5a6e0fce6b42c5aaa94681bc31de53a4731e91
artur kappel <arturvkp@gmail.com>
2022-09-21 11:33:01 -0300

    Update PokemonDetails Type

--- src/models/PokemonDetail.ts
+++ src/models/PokemonDetail.ts
@@ -1,31 +1,26 @@
 export type PokemonDetailsResult = {
-  stats: [
-    {
-      base_stat: number;
-      stat: {
-        name:
-          | "hp"
-          | "attack"
-          | "defense"
-          | "special-attack"
-          | "special-defense"
-          | "speed";
-      };
-    }
-  ];
-  types: [
-    {
-      slot: number;
-      type: {
-        name: string;
-      };
-    }
-  ];
-  abilities: [
-    {
-      ability: {
-        name: string;
-      };
-    }
-  ];
+  stats: {
+    base_stat: number;
+    stat: {
+      name:
+        | "hp"
+        | "attack"
+        | "defense"
+        | "special-attack"
+        | "special-defense"
+        | "speed";
+    };
+  }[];
+  types: {
+    slot: number;
+    type: {
+      name: string;
+    };
+  }[];
+  abilities: {
+    ability: {
+      name: string;
+    };
+  }[];
+  weight: number;
 };
