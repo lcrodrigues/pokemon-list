da6b3bb76a590f6797ee84ecc2bee982598b8551
Leonardo Rodrigues <leonardo.rodrigues@poatek.com>
2022-09-21 16:03:55 -0300

    Populate FavoritesScreen with favorites

--- src/screens/FavoritesScreen.tsx
+++ src/screens/FavoritesScreen.tsx
@@ -1,20 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import React from "react";
+import { useSelector } from "react-redux";
+import { getFavorites } from "../store/pokemon/pokemonListSlice";
+import Cell from "../components/Cell";
+import { useNavigation } from "@react-navigation/native";
+import { MainProps } from "../navigation/MainStackNavigator";
+import { PokemonResult } from "../models/PokemonResult";
 
 const FavoritesScreen = () => {
+  const navigation = useNavigation<MainProps["navigation"]>();
+  const favoriteList = useSelector(getFavorites);
+
+  const navigateToDetails = (pokemon: PokemonResult) => {
+    navigation.navigate("DetailsScreen", { pokemon });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>FavoritesScreen</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      data={favoriteList}
+      renderItem={({ item, index }) => (
+        <Cell
+          onPress={() => navigateToDetails(item)}
+          index={index}
+          pokemon={item}
+        />
+      )}
+    />
   );
 };
 
 export default FavoritesScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  list: {
+    margin: 16,
   },
 });
