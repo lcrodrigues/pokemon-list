73d9958b396ad6395ccb66b14aadc4abf70f90b0
Leonardo Rodrigues <leonardo.rodrigues@poatek.com>
2022-09-22 15:38:58 -0300

    Apply persistance

--- App.tsx
+++ App.tsx
@@ -2,14 +2,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { Provider } from "react-redux";
 import MainStackNavigator from "./src/navigation/MainStackNavigator";
-import MainTabNavigator from "./src/navigation/MainTabNavigator";
 import store from "./src/store/store";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 export default function App() {
   return (
     <NavigationContainer>
       <Provider store={store}>
-        <MainStackNavigator />
+        <PersistGate persistor={persistor}>
+          <MainStackNavigator />
+        </PersistGate>
       </Provider>
     </NavigationContainer>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,24 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import pokemonListSlice from "./pokemon/pokemonListSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
 //combine all reducers specified by our individual slices
 const rootReducer = combineReducers({
   pokemonList: pokemonListSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 //set reducer to our store
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
 
 //create RootState type based in our rootReducer
