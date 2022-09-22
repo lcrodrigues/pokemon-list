8e3d158e6f50173ff8ffae23a5b954962f7e707b
artur kappel <arturvkp@gmail.com>
2022-09-22 14:31:28 -0300

    Add text in detail screen

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -7,10 +7,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
 import { fetchPokemonDetails } from "../store/pokemon/pokemonDetailSlice";
 
-type OrderedStats = {
-  baseStat: number;
-  name: string;
-};
 const Container = styled.View`
   flex: 1;
   align-items: center;
@@ -27,6 +23,24 @@ const Pokemon = styled.Image`
   height: 100%;
 `;
 
+const TextContainer = styled.View`
+  position: absolute;
+  z-index: 4;
+
+  bottom: 0;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 12px 36px;
+  width: 100%;
+`;
+const PokemonName = styled.Text`
+  font-size: 18px;
+  color: black;
+`;
+
 const DetailsScreen = ({ route }: DetailsProps) => {
   const name = route.params.pokemon.name;
   const url = route.params.pokemon.url;
@@ -38,10 +52,16 @@ const DetailsScreen = ({ route }: DetailsProps) => {
 
   const screenState = useSelector((state: RootState) => state.pokemonDetail);
   const { abilities, stats, types, weight, sprites } = screenState.detail;
+  const [firstType] = types;
   const image = sprites.front_default;
   return (
     <Container>
       <ImageContainer>
+        <TextContainer>
+          <Text> {firstType.type.name} </Text>
+          <PokemonName> {name} </PokemonName>
+          <Text> {weight} </Text>
+        </TextContainer>
         <Pokemon
           source={{
             uri: `${image}`,
@@ -52,10 +72,9 @@ const DetailsScreen = ({ route }: DetailsProps) => {
       {stats.map((stat, index) => {
         return (
           <>
-            <Text>{stat.base_stat}</Text>
             <StatsBar
               key={index}
-              setWidth={stat.base_stat}
+              statsValue={stat.base_stat}
               statsName={stat.stat.name}
             />
           </>
